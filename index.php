13c4b3a2174e0e7baa5755cb523726ad71297b5b
wonder32 <s.schotvanger@gmail.com>
2015-02-06 13:09:41 +0100

    layout

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
+require_once '/Twig/Autoloader.php';
+Twig_Autoloader::register();
 
+$loader = new Twig_Loader_Filesystem('./view');
+$twig = new Twig_Environment($loader, array(
+    'cache' => '/compilation_cache', 'debug' => true, 'auto_reload' => true
+));
 
+$template = $twig->loadTemplate('profiel.twig');
+
+echo $template->render(array('name' => 'variables', 'go' => 'here'));
