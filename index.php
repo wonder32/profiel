07c4fa3bf1807d5c526224f6762d66c5c4c5ace1
wonder32 <s.schotvanger@gmail.com>
2015-02-07 09:42:50 +0100

    deploy

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once './Twig/Autoloader.php';
+require_once 'Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('./view');
 $twig = new Twig_Environment($loader, array(
-    'cache' => '/compilation_cache', 'debug' => true, 'auto_reload' => true
+    'debug' => true, 'auto_reload' => true
 ));
 
 $template = $twig->loadTemplate('profiel.twig');
